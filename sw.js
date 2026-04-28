f66f9165b389da86f224d85d1f376f17964a21f9
easewanki <easewanki@gmail.com>
2026-04-28 12:44:10 +0800

    Bump service worker cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "poise-click-v2";
+const CACHE_NAME = "poise-click-v3";
 const REMINDER_PATH = "/poise-reminders.ics";
 const REMINDER_ICS = `BEGIN:VCALENDAR
 VERSION:2.0
@@ -90,10 +90,10 @@ END:VCALENDAR`;
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260427r2",
-  "./app.js?v=20260427r2",
-  "./manifest.webmanifest?v=20260427r2",
-  "./icon.svg?v=20260427r2"
+  "./styles.css?v=20260428r1",
+  "./app.js?v=20260428r1",
+  "./manifest.webmanifest?v=20260428r1",
+  "./icon.svg?v=20260428r1"
 ];
 
 self.addEventListener("install", (event) => {
